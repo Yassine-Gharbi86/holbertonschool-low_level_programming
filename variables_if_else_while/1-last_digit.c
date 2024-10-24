18783a7ab13d8203c47f93a5ed3a5e5e3a7a6219
root <root@Islem.localdomain>
2024-10-24 11:44:59 +0100

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,10 +19,10 @@ last_digit = n % 10;
 if (last_digit > 5)
 {printf(" and is greater than 5");
 }
-if (last_digit == 0)
+else if (last_digit == 0)
 {printf(" and is 0");
 }
-if (last_digit < 6 && last_digit != 0)
+else if(last_digit < 6 && last_digit != 0)
 {printf(" and is less than 6 and not 0");
 }
 return (0);
