b6a30b37bf9c2ab53dc7540853fdd8fc182586fd
root <root@DESKTOP-NDVQC7F.localdomain>
2024-11-02 15:31:07 +0100

    task5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - check the code
+ * Description: change all lower cases to uppercase
+ * @s: pointer to the string to be executed
+ * Return: void
+ */
+char *string_toupper(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
+i++;
+}
+return (s);
+}
