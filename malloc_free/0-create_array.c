6e6b428eff981e0f8edda687858f44792b2791ed
root <root@Islem.localdomain>
2024-11-13 11:19:40 +0100

    fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "main.h"
 /**
  * create_array - creates an array of chars initialized with a specific char
  * @size: size of the array to create
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "main.h"
 /**
  * str_concat - Concatenates two strings.
  * @s1: The first string.
