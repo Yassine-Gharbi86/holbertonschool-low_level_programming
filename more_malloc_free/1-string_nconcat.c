9dbedc7fd3325784928089e577493d7967455feb
root <root@Islem.localdomain>
2024-11-14 12:05:24 +0100

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,24 +1,33 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
- * string_nconcat - concatenates two strings
- * @s1: The first string to concatenate
- * @s2: The second string to concatenate
- * @n: The maximum number of bytes to concatenate from s2
- *
- * Return: A pointer to the concatenated string, or NULL if it fails
+ * string_nconcat - Concatenates the first `n` bytes of string `s2`
+ * to the end of string `s1`.
+ * @s1: the first string.
+ * @s2: the second string.
+ * @n: the number of bytes from `s2` to concatenate.
+ * Return: a pointer to the newly allocated string,
+ *or NULL if allocation fails.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-size_t len1 = strlen(s1 ? s1 : "");
-size_t len2 = strlen(s2 ? s2 : "");
-size_t len_n = (n > len2) ? len2 : n;
-char *concat = malloc(len1 + len_n + 1);
-if (!concat)
+char *concat;
+unsigned int len = n, index;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (index = 0; s1[index]; index++)
+len++;
+concat = malloc(sizeof(char) * (len + 1));
+if (concat == NULL)
 return (NULL);
-strcpy(concat, s1);
-strncat(concat, s2, len_n);
+len = 0;
+for (index = 0; s1[index]; index++)
+concat[len++] = s1[index];
+for (index = 0; s2[index] && index < n; index++)
+concat[len++] = s2[index];
+concat[len] = '\0';
 return (concat);
 }
