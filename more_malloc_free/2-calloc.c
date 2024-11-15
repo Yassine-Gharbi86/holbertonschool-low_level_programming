4d77fb0f84fc45676ffd6c1d3c533e45858953ff
root <root@Islem.localdomain>
2024-11-15 11:08:45 +0100

    task

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,11 +11,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *block;
+unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 block = malloc(nmemb * size);
-if (block == NULL)
-return (NULL);
-memset(block, 0, nmemb * size);
+if (block != NULL)
+{
+for (i = 0; i < (nmemb * size); i++)
+block[i] = 0;
 return (block);
+}
+else
+return (NULL);
 }
