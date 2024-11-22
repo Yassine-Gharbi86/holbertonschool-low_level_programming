8b13f79142ced62af6001f73ce18703300d2fa32
root <root@Islem.localdomain>
2024-11-22 14:00:37 +0100

    all

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+* print_numbers - Prints numbers with a separator
+* @n: Number of arguments
+* @separator: The separator between numbers
+*
+* Return: Prints the numbers with the separator
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(args, int));
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+va_end(args);
+printf("\n");
+}
---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+* print_strings - Prints strings followed by a newline
+* @separator: The string between the strings
+* @n: The number of strings
+* Return: Nothing
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+char *str;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+str = va_arg(args, char *);
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s", str);
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+va_end(args);
+printf("\n");
+}
