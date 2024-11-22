5f8720824bd7e6593c4d5e15c3c101ba0f97bd0e
root <root@Islem.localdomain>
2024-11-22 14:18:21 +0100

    0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,11 +1,10 @@
 #include "variadic_functions.h"
-
 /**
- * sum_them_all - Returns the sum of all its parameters.
- * @n: Number of arguments passed to the function.
- *
- * Return: Sum of parameters, or 0 if n == 0.
- */
+* sum_them_all - Returns the sum of all its parameters.
+* @n: Number of arguments passed to the function.
+*
+* Return: Sum of parameters, or 0 if n == 0.
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
