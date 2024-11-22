4b4c37d5181af319a73de0246ae1d1c9233f0cb4
root <root@Islem.localdomain>
2024-11-22 12:21:59 +0100

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
- * print_name - Calls a function to print a name
- * @name: The name to be printed
- * @f: A pointer to the function to use for printing
- *
- * Return: Nothing
- */
+* print_name - Calls a function to print a name
+* @name: The name to be printed
+* @f: A pointer to the function to use for printing
+*
+* Return: Nothing
+*/
 void print_name(char *name, void (*f)(char *))
 {
 if (name != NULL && f != NULL)
