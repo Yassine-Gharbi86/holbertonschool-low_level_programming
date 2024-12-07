97377645060b9bd36aadb8fef8eebd02466ad0cd
root <root@DESKTOP-NDVQC7F.localdomain>
2024-12-07 14:52:59 +0100

    0-binary_to_uint.c and main.h added

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* binary_to_uint - converts a binary number to an unsigned int.
+* @b: string containing the binary number.
+* Return: the converted number, or 0 if the string is invalid.
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int number = 0;
+int i;
+if (b == NULL)
+return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+number = number * 2 + (b[i] - '0');
+}
+return (number);
+}
