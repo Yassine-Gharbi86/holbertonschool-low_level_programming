5372e50fadd00e8114228daad59e1be4d395dae8
root <root@DESKTOP-NDVQC7F.localdomain>
2024-12-08 15:09:57 +0100

    3-cp.c updated

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,97 +1,66 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#define BUFFER_SIZE 1024
 
 /**
-* check_arguments - check if the correct number of arguments is passed
-* @argc: argument count
+* error_file - checks if files can be opened.
+* @source_fd: file descriptor for the source file.
+* @dest_fd: file descriptor for the destination file.
+* @argv: arguments vector.
+* Return: no return.
 */
-void check_arguments(int argc)
+void error_file(int source_fd, int dest_fd, char *argv[])
 {
-if (argc != 3)
+if (source_fd == -1)
 {
-dprintf(2, "Usage: cp file_from file_to\n");
-exit(97);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
-}
-/**
-* open_file - open a file and return the file descriptor
-* @filename: name of the file to open
-* @flags: the flags for open() (e.g., O_RDONLY, O_WRONLY)
-* @mode: the mode to set when creating a file (if applicable)
-* @is_input: flag to check if it's input (read) or output (write)
-*
-* Return: file descriptor on success, -1 on failure
-*/
-int open_file(const char *filename, int flags, mode_t mode, int is_input)
+if (dest_fd == -1)
 {
-int fd = open(filename, flags, mode);
-if (fd == -1)
-{
-if (is_input)
-dprintf(2, "Error: Can't read from file %s\n", filename);
-else
-dprintf(2, "Error: Can't write to %s\n", filename);
-exit(is_input ? 98 : 99);
-}
-return (fd);
-}
-/**
-* copy_content - copy content from one file to another
-* @file_from: file descriptor for the source file
-* @file_to: file descriptor for the destination file
-*/
-void copy_content(int file_from, int file_to)
-{
-ssize_t nread, nwrite;
-char buffer[BUFFER_SIZE];
-while ((nread = read(file_from, buffer, BUFFER_SIZE)) > 0)
-{
-nwrite = write(file_to, buffer, nread);
-if (nwrite == -1)
-{
-dprintf(2, "Error: Can't write to %s\n", "file_to");
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
-if (nread == -1)
-{
-dprintf(2, "Error: Can't read from file %s\n", "file_from");
-exit(98);
-}
-}
 /**
-* close_file - close a file and handle errors
-* @fd: file descriptor to close
-*/
-void close_file(int fd)
-{
-if (close(fd) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
-}
-/**
-* main - copies the content of one file to another
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: Always 0 on success, appropriate exit code on failure
+* main - check the code for Holberton School students.
+* @argc: number of arguments.
+* @argv: arguments vector.
+* Return: Always 0.
 */
 int main(int argc, char *argv[])
 {
-int file_from, file_to;
-check_arguments(argc);
-file_from = open_file(argv[1], O_RDONLY, 0, 1);
-file_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664, 0);
-copy_content(file_from, file_to);
-close_file(file_from);
-close_file(file_to);
+int source_fd, dest_fd, close_status;
+ssize_t bytes_read, bytes_written;
+char buffer[1024];
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+exit(97);
+}
+source_fd = open(argv[1], O_RDONLY);
+dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+error_file(source_fd, dest_fd, argv);
+bytes_read = 1024;
+while (bytes_read == 1024)
+{
+bytes_read = read(source_fd, buffer, 1024);
+if (bytes_read == -1)
+error_file(-1, 0, argv);
+bytes_written = write(dest_fd, buffer, bytes_read);
+if (bytes_written == -1)
+error_file(0, -1, argv);
+}
+close_status = close(source_fd);
+if (close_status == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd);
+exit(100);
+}
+close_status = close(dest_fd);
+if (close_status == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
+exit(100);
+}
 return (0);
 }
